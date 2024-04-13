d5679e92c794febf2513dfc8fc823f2806414ef3
Krister Walfridsson <krister.walfridsson@gmail.com>
2024-04-13 13:12:40 +0200

    Remove all instructions from always UB BBs
    Remove all instructions from basic blocks that are always UB. This
    helps simplify some cases with loops such as:
    
      int a[5][5];
      void bar(int n)
      {
        for (int i = 0; i < n; i++)
          for (int j = 0; j < n; j++)
            a[i][j] += 1;
      }
    
    where unrolling results in many BBs that are always UB.

--- lib/dead_code_elimination.cpp
+++ lib/dead_code_elimination.cpp
@@ -1,5 +1,7 @@
 // Remove trivially dead instructions.
 
+#include <cassert>
+
 #include "smtgcc.h"
 
 namespace smtgcc {
@@ -25,18 +27,75 @@ void destroy(Instruction *inst)
   destroy_instruction(inst);
 }
 
+// Remove all instructions (except the "ub 1") for a basic block that
+// is always UB.
+//
+// Note: This should only be called for loop-free functions because
+// we may otherwise create infinite loops where there is no path to the
+// exit block if a removed instruction was used in a branch condition.
+void clear_ub_bb(Basic_block *bb)
+{
+  if (bb->last_inst->opcode == Op::BR && bb->last_inst->nof_args == 1)
+    {
+      // Change the conditional branch to an unconditional branch. It does
+      // not matter which branch we take since the execution is UB anyway.
+      Instruction *cond = bb->last_inst->arguments[0];
+      cond->replace_use_with(bb->last_inst, bb->value_inst(1, 1));
+    }
+
+  bool found_ub = false;
+  for (Instruction *inst = bb->last_inst->prev; inst;)
+    {
+      Instruction *next_inst = inst->prev;
+      if (inst->opcode == Op::UB && is_true(inst->arguments[0]))
+	{
+	  if (found_ub)
+	    destroy(inst);
+	  found_ub = true;
+	}
+      else if(inst->has_lhs())
+	{
+	  if (!inst->used_by.empty())
+	    inst->replace_all_uses_with(bb->value_inst(0, inst->bitsize));
+	  destroy(inst);
+	}
+      else
+	destroy(inst);
+      inst = next_inst;
+    }
+  assert(found_ub);
+
+  while (!bb->phis.empty())
+    {
+      Instruction *inst = bb->phis.back();
+      if (!inst->used_by.empty())
+	inst->replace_all_uses_with(bb->value_inst(0, inst->bitsize));
+      destroy(inst);
+    }
+}
+
 } // end anonymous namespace
 
 void dead_code_elimination(Function *func)
 {
+  bool is_loopfree = !has_loops(func);
   for (int i = func->bbs.size() - 1; i >= 0; i--)
     {
       Basic_block *bb = func->bbs[i];
+
+      // Remove dead instructions.
       for (Instruction *inst = bb->last_inst; inst;)
 	{
 	  Instruction *next_inst = inst->prev;
 	  if (inst->has_lhs() && inst->used_by.empty())
 	    destroy(inst);
+	  else if (is_loopfree &&
+		   bb != func->bbs[0]
+		   && inst->opcode == Op::UB && is_true(inst->arguments[0]))
+	    {
+	      clear_ub_bb(bb);
+	      break;
+	    }
 	  else if (inst->opcode == Op::UB && is_false(inst->arguments[0]))
 	    destroy(inst);
 	  else if (inst->opcode == Op::ASSERT && is_true(inst->arguments[0]))
@@ -44,6 +103,7 @@ void dead_code_elimination(Function *func)
 	  inst = next_inst;
 	}
 
+      // Remove dead phi-nodes.
       std::vector<Instruction *> dead_phis;
       for (auto phi : bb->phis)
 	{
