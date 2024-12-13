2a9bf591a40a7390f32f9993cf776b5f099e09c5
Krister Walfridsson <krister.walfridsson@gmail.com>
2024-12-13 05:14:41 +0100

    Do not eliminate all instructions for UB block
    The pass used to remove all instructions for basic blocks that
    were always UB. This had the bad side effect of always changing
    the result for code entering an always UB block, and therefore
    makes smtgcc less useful as it is harder to identify issues that
    may generate wrong code as compared to cases that only introduce
    a new harmless integer overflow.
    
    We now keep all instructions, even for always UB basic blocks.

--- lib/dead_code_elimination.cpp
+++ lib/dead_code_elimination.cpp
@@ -27,83 +27,14 @@ void destroy(Inst *inst)
   destroy_instruction(inst);
 }
 
-// Remove all instructions (except the "ub 1") for a basic block that
-// is always UB.
-void clear_ub_bb(Basic_block *bb, bool is_loopfree)
-{
-  if (is_loopfree
-      && bb->last_inst->op == Op::BR && bb->last_inst->nof_args == 1)
-    {
-      // Change the conditional branch to an unconditional branch. It does
-      // not matter which branch we take since the execution is UB anyway.
-      Inst *cond = bb->last_inst->args[0];
-      cond->replace_use_with(bb->last_inst, bb->value_inst(1, 1));
-    }
-
-  Inst *found_ub = nullptr;
-  for (Inst *inst = bb->last_inst->prev; inst;)
-    {
-      Inst *next_inst = inst->prev;
-      if (inst->op == Op::UB && is_true(inst->args[0]))
-	{
-	  if (found_ub)
-	    destroy(inst);
-	  else
-	    found_ub = inst;
-	}
-      else if(inst->has_lhs() && !inst->used_by.empty())
-	{
-	  if (is_loopfree)
-	    {
-	      inst->replace_all_uses_with(bb->value_inst(0, inst->bitsize));
-	      destroy(inst);
-	    }
-	}
-      else
-	destroy(inst);
-      inst = next_inst;
-    }
-  assert(found_ub);
-
-  while (!bb->phis.empty())
-    {
-      Inst *inst = bb->phis.back();
-      if (!inst->used_by.empty())
-	inst->replace_all_uses_with(bb->value_inst(0, inst->bitsize));
-      destroy(inst);
-    }
-
-  if (bb->first_inst != found_ub)
-    found_ub->move_before(bb->first_inst);
-}
-
 } // end anonymous namespace
 
 void dead_code_elimination(Function *func)
 {
-  bool is_loopfree = !has_loops(func);
   uint32_t nof_inst = 0;
   for (int i = func->bbs.size() - 1; i >= 0; i--)
     {
       Basic_block *bb = func->bbs[i];
-
-      // Propagate "always UB" from successors (this BB is always UB if
-      // all its successors are always UB).
-      if (bb != func->bbs[0]
-	  && !bb->succs.empty()
-	  && (bb->first_inst->op != Op::UB
-	      || !is_true(bb->first_inst->args[0])))
-	{
-	  bool succs_are_ub = true;
-	  for (auto succ : bb->succs)
-	    {
-	      succs_are_ub =
-		succs_are_ub && succ->first_inst->op == Op::UB
-		&& is_true(succ->first_inst->args[0]);
-	    }
-	  if (succs_are_ub)
-	    bb->build_inst(Op::UB, bb->value_inst(1, 1));
-	}
 
       // Remove dead instructions.
       for (Inst *inst = bb->last_inst; inst;)
@@ -111,12 +42,6 @@ void dead_code_elimination(Function *func)
 	  Inst *next_inst = inst->prev;
 	  if (inst->has_lhs() && inst->used_by.empty())
 	    destroy(inst);
-	  else if (bb != func->bbs[0]
-		   && inst->op == Op::UB && is_true(inst->args[0]))
-	    {
-	      clear_ub_bb(bb, is_loopfree);
-	      break;
-	    }
 	  else if (inst->op == Op::UB && is_false(inst->args[0]))
 	    destroy(inst);
 	  else if (inst->op == Op::ASSERT && is_true(inst->args[0]))
@@ -141,30 +66,6 @@ void dead_code_elimination(Function *func)
 
   if (nof_inst > max_nof_inst)
     throw Not_implemented("too many instructions");
-
-  for (auto bb : func->bbs)
-    {
-      // Propagate "always UB" from predecessors (this BB is always UB if
-      // all its predecessors are always UB).
-      if (bb != func->bbs.back()
-	  && !bb->preds.empty()
-	  && (bb->first_inst->op != Op::UB
-	      || !is_true(bb->first_inst->args[0])))
-	{
-	  bool preds_are_ub = true;
-	  for (auto pred : bb->preds)
-	    {
-	      preds_are_ub =
-		preds_are_ub && pred->first_inst->op == Op::UB
-		&& is_true(pred->first_inst->args[0]);
-	    }
-	  if (preds_are_ub)
-	    {
-	      bb->build_inst(Op::UB, bb->value_inst(1, 1));
-	      clear_ub_bb(bb, is_loopfree);
-	    }
-	}
-    }
 }
 
 void dead_code_elimination(Module *module)
