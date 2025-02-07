a5ba2ee6ad315dc0838705082469ea9aa523b90e
Krister Walfridsson <krister.walfridsson@gmail.com>
2025-02-07 21:10:21 +0100

    aarch64: bugfix whilelo
    Only the least significant bit of each element in the predicate is
    set to 1. For example
    
        mov      w0, 4
        whilelo  p0.s, wzr, w0
    
    sets p0 to 0x1111.

--- lib/read_aarch64.cpp
+++ lib/read_aarch64.cpp
@@ -4511,11 +4511,13 @@ void Parser::process_sve_while()
 
   Inst *res = nullptr;
   Inst *one = bb->value_inst(1, arg1->bitsize);
+  Inst *last_cmp = nullptr;
   for (uint32_t i = 0; i < nof_elem; i++)
     {
       Inst *inst = bb->build_inst(Op::ULT, arg1, arg2);
+      last_cmp = inst;
       if (elem_bitsize > 8)
-	inst = bb->build_inst(Op::SEXT, inst, elem_bitsize / 8);
+	inst = bb->build_inst(Op::ZEXT, inst, elem_bitsize / 8);
       if (res)
 	res = bb->build_inst(Op::CONCAT, inst, res);
       else
@@ -4527,7 +4529,7 @@ void Parser::process_sve_while()
   bb->build_inst(Op::WRITE, rstate->registers[Aarch64RegIdx::n], n);
   Inst *z = bb->build_inst(Op::EQ, res, bb->value_inst(0, res->bitsize));
   bb->build_inst(Op::WRITE, rstate->registers[Aarch64RegIdx::z], z);
-  Inst *c = bb->build_inst(Op::NOT, extract_vec_elem(res, 1, 15));
+  Inst *c = bb->build_inst(Op::NOT, last_cmp);
   bb->build_inst(Op::WRITE, rstate->registers[Aarch64RegIdx::c], c);
   Inst *v = bb->value_inst(0, 1);
   bb->build_inst(Op::WRITE, rstate->registers[Aarch64RegIdx::v], v);
