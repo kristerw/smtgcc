815d21537c1f9adfdf676d922525cefe475ff22f
Krister Walfridsson <krister.walfridsson@gmail.com>
2025-11-08 19:08:49 +0100

    aarch64: Implement nots

--- lib/read_aarch64.cpp
+++ lib/read_aarch64.cpp
@@ -301,7 +301,7 @@ private:
   void process_sve_binary_rev(Op op);
   void process_sve_binary(Inst*(*gen_elem)(Basic_block*, Inst*, Inst*));
   void process_sve_ternary(Inst*(*gen_elem)(Basic_block*, Inst*, Inst*, Inst*));
-  void process_sve_preg_unary(Op op);
+  void process_sve_preg_unary(Op op, bool update_nzcv = false);
   void process_sve_preg_binary(Op op, bool perform_not = false);
   void process_sve_preg_binary(Inst*(*gen_elem)(Basic_block*, Inst*, Inst*));
   void process_sve_preg_punpk(bool high);
@@ -5813,7 +5813,7 @@ void Parser::process_sve_ternary(Inst*(*gen_elem)(Basic_block*, Inst*, Inst*, In
   write_reg(dest, res);
 }
 
-void Parser::process_sve_preg_unary(Op op)
+void Parser::process_sve_preg_unary(Op op, bool update_nzcv)
 {
   auto [dest, nof_elem, elem_bitsize] = get_preg(1);
   get_comma(2);
@@ -5823,19 +5823,41 @@ void Parser::process_sve_preg_unary(Op op)
   get_end_of_line(6);
 
   Inst *res = nullptr;
+  Inst *last = bb->value_inst(0, 1);
+  Inst *none = bb->value_inst(1, 1);
+  std::vector<std::pair<Inst *, Inst *>> first_elems;
+  first_elems.reserve(nof_elem);
   for (uint32_t i = 0; i < nof_elem; i++)
     {
-      Inst *elem1 = extract_pred_elem(arg1, elem_bitsize, i);
-      Inst *inst = bb->build_inst(op, elem1);
-
       Inst *pred_elem = extract_pred_elem(pred, elem_bitsize, i);
-      inst = bb->build_inst(Op::ITE, pred_elem, inst, bb->value_inst(0, 1));
+      Inst *elem = extract_pred_elem(arg1, elem_bitsize, i);
+      Inst *inst = bb->build_inst(op, elem);
+      last = bb->build_inst(Op::ITE, pred_elem, inst, last);
+      first_elems.push_back({pred_elem, inst});
+      inst = bb->build_inst(Op::AND, inst, pred_elem);
+      none = bb->build_inst(Op::AND, none, bb->build_inst(Op::NOT, inst));
       if (elem_bitsize > 8)
 	inst = bb->build_inst(Op::ZEXT, inst, elem_bitsize / 8);
       if (res)
 	res = bb->build_inst(Op::CONCAT, inst, res);
       else
 	res = inst;
+    }
+
+  if (update_nzcv)
+    {
+      Inst *first = bb->value_inst(0, 1);
+      while (!first_elems.empty())
+	{
+	  auto [pred, inst] = first_elems.back();
+	  first_elems.pop_back();
+	  first = bb->build_inst(Op::ITE, pred, inst, first);
+	}
+      Inst *n = first;
+      Inst *z = none;
+      Inst *c = bb->build_inst(Op::NOT, last);
+      Inst *v = bb->value_inst(0, 1);
+      set_nzcv(n, z, c, v);
     }
   write_reg(dest, res);
 }
@@ -6735,6 +6757,8 @@ void Parser::parse_sve_preg_op()
     process_sve_preg_binary(gen_nor);
   else if (name == "not")
     process_sve_preg_unary(Op::NOT);
+  else if (name == "nots")
+    process_sve_preg_unary(Op::NOT, true);
   else if (name == "orn")
     process_sve_preg_binary(Op::OR, true);
   else if (name == "orr")
